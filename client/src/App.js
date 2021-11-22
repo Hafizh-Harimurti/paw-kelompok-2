2ac79dfb9b57280420262a4d5bd078637c7f4ecf
Syafiq-ma <alaudinsyafiq910@gmail.com>
2021-11-22 17:54:00 +0700

    configure react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+
+import { Routes, Route } from "react-router-dom";
+
 import './App.css';
 
-function App() {
+import Patient from './page/Patient';
+import Queueui from './Queueui';
+
+function App(){
   return (
-    <div className="App">
-      Hello there
-    </div>
-  );
+    <Routes>
+      <Route path="/patient/*" element={<Patient/>}/>
+      <Route path="/queue/*" element={<Queueui/>}/>
+    </Routes>
+  )
 }
 
 export default App;
