1661d33d873743f8d0d4cf930770fbf06492f65a
arifahamelia <arifahameliatijani91@gmail.com>
2021-11-23 12:22:49 +0700

    Add PatientForm UI

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import './App.css';
 
 import Patient from './page/Patient';
 import Queueui from './page/Queue';
+import PatientForm from './page/PatientForm';
 
 function App(){
   return (
     <Routes>
       <Route path="/patient/*" element={<Patient/>}/>
       <Route path="/queue/*" element={<Queueui/>}/>
+      <Route path="/patient2/*" element={<PatientForm/>}/>
     </Routes>
   )
 }
