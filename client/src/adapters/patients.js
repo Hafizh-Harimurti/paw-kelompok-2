70667aa0d4a165fa0f087de23c3595f7ca88516b
YosepEChristo <yosepechristo@gmail.com>
2021-11-23 16:30:42 +0700

    commit on 'tugas 2'

--- client/src/adapters/patients.js
+++ client/src/adapters/patients.js
@@ -37,3 +37,40 @@ export const deletePatient = (patientData) => {
         .catch(err)
     return res
 };
+
+const queueApiPath = "https://localhost:3000/api/queue"
+
+export const getQueue = (queueData) => {
+    let res = Axios.get(queueApiPath)
+    .then(response=>{
+        response.data
+    })
+    .catch(err)
+    return res
+};
+
+export const postQueue = (queuetData) => {
+    let res = Axios.post(queueApiPath, queueData)
+        .then(response=>{
+            response.data
+        })
+        .catch(err)
+    return res
+};
+
+export const putQueue = (queuetData) => {
+    let res = Axios.put(queueApiPath, queueData)
+        .then(response=>{
+            response.data
+        })
+        .catch(err)
+    return res
+};
+export const deleteQueue = (queuetData) => {
+    let res = Axios.delete(queueApiPath, queueData)
+        .then(response=>{
+            response.data
+        })
+        .catch(err)
+    return res
+};
--- client/src/components/QueueAdd.js
+++ client/src/components/QueueAdd.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { createTheme, Grid, TextField, Button, Card, CardContent, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
 
+function QueueAdd( {queueAdd, error} ) {
+    
+    const [queuedetails, setqueueDetails] = React.useState({
+        ownername: "", 
+        checkdate: "", 
+        petname: "", 
+        pettype: "", 
+        homeaddress: "", 
+        phonenumber: ""});
 
-function QueueAdd() {
+        const submitHandler = e => {
+            e.preventDefault();
+            QueueAdd(queuedetails);
+        }
 
     const theme = createMuiTheme({
         typography: {
@@ -23,31 +36,42 @@ function QueueAdd() {
                     Add Queue
                 </Typography> 
 
-                    <form>
+                    <form onSubmit={submitHandler}>
                     <Grid container spacing={1}>
                         <Grid item xs={12}>
-                        <TextField 
+                        <TextField id="checkdate" name="checkdate"
                         InputLabelProps={{shrink: true,}}
-                        placeholder="Date" label="Date" variant="standard" fullWidth required type="date" />
+                        placeholder="Date" label="Date" variant="standard" fullWidth required type="date" 
+                        onChange= {e=> setqueueDetails({...queuedetails, checkdate: e.target.value})} value={queuedetails.checkdate} />
                         </Grid>
+
                         <Grid item xs={12}>
-                        <TextField placeholder="Enter Owner Name" label="Owner Name" variant="standard" fullWidth required />
+                        <TextField id="ownername" name="ownername" placeholder="Enter Owner Name" label="Owner Name" variant="standard" 
+                        onChange= {e=> setqueueDetails({...queuedetails, ownername: e.target.value})} value={queuedetails.ownername} fullWidth required  />
                         </Grid>
+
                         <Grid item xs={12}>
-                        <TextField placeholder="Enter Pet Name" label="Pet Name" variant="standard" fullWidth required />
+                        <TextField id="petname" name="petname" placeholder="Enter Pet Name" label="Pet Name" variant="standard" 
+                        onChange= {e=> setqueueDetails({...queuedetails, petname: e.target.value})} value={queuedetails.petname} fullWidth required />
                         </Grid>
+
                         <Grid item xs={12}>
-                        <TextField placeholder="Enter Pet Type" label="Pet Type" variant="standard" fullWidth required />
+                        <TextField id="pettype" name="pettype" placeholder="Enter Pet Type" label="Pet Type" variant="standard" 
+                        onChange= {e=> setqueueDetails({...queuedetails, pettype: e.target.value})} value={queuedetails.pettype} fullWidth required />
                         </Grid>
+
                         <Grid item xs={12}>
-                        <TextField placeholder="Enter Home Address" label="Home Address" variant="standard" fullWidth required />
-                        </Grid>
-                        <Grid item xs={12}>
-                        <TextField placeholder="Enter Phone Number" label="Phone Number" variant="standard" fullWidth required />
+                        <TextField id="homeadress" name="homeadress" placeholder="Enter Home Address" label="Home Address" variant="standard" 
+                        onChange= {e=> setqueueDetails({...queuedetails, homeaddress: e.target.value})} value={queuedetails.homeaddress} fullWidth required />
                         </Grid>
                         
                         <Grid item xs={12}>
-                        <Button type="submit" variant="contained" color="primary" fullWidth style={{ backgroundColor: '#00CC99', color: '#FFFFFF', textTransform: 'none'}}>
+                        <TextField id="phonenumber" name="phonenumber" placeholder="Enter Phone Number" label="Phone Number" variant="standard" 
+                        onChange= {e=> setqueueDetails({...queuedetails, phonenumber: e.target.value})} value={queuedetails.phonenumber} fullWidth required />
+                        </Grid>
+                        
+                        <Grid item xs={12}>
+                        <Button type="submit" variant="contained" color="primary" value="QUEUEDETAILS" fullWidth style={{ backgroundColor: '#00CC99', color: '#FFFFFF', textTransform: 'none'}}>
                             <Typography>Add</Typography></Button>
                         </Grid>
 
