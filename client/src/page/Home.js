b399755138b9928a5dab514f86c8d0408e8c9484
Hafizh Aradhana Harimurti <mochikacang1@mail.ugm.ac.id>
2021-12-01 00:44:11 +0700

    Added navigation to home screen's button and tidied up project folder

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import Navbar from "../components/Navbar";
 import VetIlustration from "../assets/vet_ilustration.jpg"
 import "../styles/home.css"
 
 const Home = () => {
+    let navigate = useNavigate()
+
+    const redirectToQueue = () =>{
+        navigate('/queue')
+    }
+
     return(
         <div>
             <Navbar/>
@@ -14,7 +21,7 @@ const Home = () => {
                     <div className="homeContent">
                         Veterinary adalah sebuah Aplikasi yang memudahkan masyarakat untuk mendatangi dokter hewan. Masyarakat dapat mendaftar ke dokter hewan melalui aplikasi ini sehingga tidak perlu mengantri ketika hendak memeriksakan hewan peliharaannya. Aplikasi ini juga memudakan masyarakat untuk melakukan pendataan kondisi hewan dan riwayat penyakit hewan peliharaannya
                     </div>
-                    <button className="homeBtn">Periksa Sekarang!</button>
+                    <button className="homeBtn" onClick={redirectToQueue}>Periksa Sekarang!</button>
                 </div>
                 <div className="Ilustration">
                     <img src={VetIlustration} alt="Veternier"></img>
