2b5d75d2a3b3252ddc189f48c37f3de04dfedead
Hafizh Aradhana Harimurti <mochikacang1@mail.ugm.ac.id>
2021-12-01 00:13:44 +0700

    Fixed cancel error

--- client/src/components/EditableQueueRow.js
+++ client/src/components/EditableQueueRow.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const EditableQueueRow = ({ editFormData, handleEditFormChange}) => {
+const EditableQueueRow = ({ editFormData, handleEditFormChange, handleCancelClick}) => {
     return (
         <tr>
             <td>
